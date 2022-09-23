f57b9621c8a449b3507d5028f36efc889c2aecda
Phill Legnitto <palegnitto@gmail.com>
2022-09-23 12:28:38 -0700

    completed merge

--- bubble.js
+++ bubble.js
@@ -1,9 +1,11 @@
 function bubbleSort(arr) {
 
-  for (let num of arr){
-    for (let i = 0; i < arr.length; i++){
-
-      if ( arr[i] > arr[i + 1]) [arr[i], arr[i + 1]] = [[arr[i+1], arr[i]]];
+  for (let j = 0; j < arr.length; j++){
+    for (let i = 0; i < arr.length-j; i++){
+      let holdMyBeer;
+      if ( arr[i] > arr[i + 1]) {
+          [arr[i],arr[i+1]] = [arr[i+1], arr[i]]
+      }
     }
   }
   return arr;
--- merge.js
+++ merge.js
@@ -1,4 +1,40 @@
-function merge() {}
+function merge(arr1,arr2) {
+  let i=0;
+  let j=0;
+  let arr3= [];
+
+  while( arr3.length !== arr1.length + arr2.length){
+    console.log('3',arr3.length,'2', arr1.length,'1', arr2.length)
+    debugger;
+    if (arr1[i] < arr2[j]){
+      arr3.push(arr1[i]);
+      i++;
+    } else if (arr1[i] > arr2[j]){
+      arr3.push(arr2[j]);
+      j++;
+
+    } else if (arr1[i] === arr2[j]){
+      arr3.push(arr1[i]);
+      arr3.push(arr2[j]);
+      i++;
+      j++;
+    }
+    if (arr1.length === i){
+      debugger;
+      arr3 = arr3.concat(arr2.slice(j));
+    } else if (arr2.length === j){
+      arr3 = arr3.concat(arr1.slice(i));
+    };
+  }
+  return arr3;
+}
+merge([2,4,5], [3,7,8])
+
+/**
+ * compare  array1[0] to array2[0]
+ * take lesser value and add to new array
+ *
+ */
 
 function mergeSort() {}
 
